90deed12a518a9c0b65684afc30470878aeaa7d6
kartickg33 <68003347+kartickg33@users.noreply.github.com>
2021-03-30 19:51:32 +0530

    deploy-3

--- app.js
+++ app.js
@@ -70,8 +70,8 @@ const sessionConfig = {
     cookie:{
         httpOnly:true,
         //secure:true,
-        expires: Date.now() + 1000*60*60*24*7,
-        maxAge:1000*60*60*24*7
+        expires: Date.now() + 1000*60*15,
+        maxAge:1000*60*15
     } 
 }
 app.use(session(sessionConfig))
